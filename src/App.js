8b4b966529469cbd29da68798724103fba25eed9
hovhannisyan2010 <hovhannisyanjora2010@gmail.com>
2024-12-27 23:04:08 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,99 +1,98 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { useState } from 'react';
+import { grey, red, purple } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
+import CardHeader from '@mui/material/CardHeader';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  height:300,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  display:"flex",
-  justifyContent:"center"
-};
 
-const btnstyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 150,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
-const inpstyle = {
-  width:200,
-  display:"flex",
-  flexDirection:"column",
-  gap:10,
-  alignItems:"center"
-};
+let color = grey[500]
 export default function App() {
-  
-  const [text,setText] = useState({
-    text1:"",
-    text2:"",
-    text3:"",
-  })
-  
-    function create(event) {
-      event.preventDefault()
-      if (event.target.text1.value !== "" && event.target.text2.value !== "") {
-        if (event.target.text3.value !== "") { 
-          setText({
-            text1: event.target.text1.value,
-            text2: event.target.text2.value,
-            text3: event.target.text3.value,
-          })
-          event.target.text1.value = ""
-          event.target.text2.value = ""
-          event.target.text3.value = ""
-          handleClose()
-        }
-      }
-    }
-    
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+  const [plus, setPlus] = React.useState(2)
+  const [sum , setSum] = React.useState(1)
+  console.log(plus);
+  const [expanded, setExpanded] = React.useState(false);
+  if (expanded) {
+    color = red[500]
+  }else{
+    color = grey[500]
+  }
   return (
-    <div>
-      <Button onClick={handleOpen} style={btnstyle}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <form onSubmit={create} style={inpstyle}>
+    <div style={{height:"100vh",display:"flex",justifyContent:"center", alignItems:"center",gap:"100px", backgroundColor:grey[500]}}>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="294"
+        image="https://th.bing.com/th/id/OIP.om3QKNYbLOSygabuihA34wHaE8?rs=1&pid=ImgDetMain"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="h4" >
+          The hotel
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing style={{display:"flex" , justifyContent:"space-between"}}>
+        <h1>180$</h1>
+        <IconButton aria-label="add to favorites" onClick={()=>{setExpanded(!expanded)}}>
+          <FavoriteIcon style={ {color}}/>
+        </IconButton>
+      </CardActions>
+      
+    </Card>
 
-              <input style={{border:"1px solid",padding:"5px",borderRadius:"10px"}} type='text' name='text1'/>
-              <input style={{border:"1px solid",padding:"5px",borderRadius:"10px"}} type='text' name='text2'/>
-              <input style={{border:"1px solid",padding:"5px",borderRadius:"10px"}} type='text' name='text3'/>
-              <button type='submit' style={{width:"90px", height:"30px"}}>click</button>
-            </form>
-          </Typography>
-        </Box>
-      </Modal>
-      <div style={{marginLeft:"10px"}}>
-        <h2>{text.text1}</h2>
-        <h2>{text.text2}</h2>
-        <h2>{text.text3}</h2>
-      </div>
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="294"
+        image="https://th.bing.com/th/id/OIP.sORUCLQs6IFavbrcEWRPgAHaE8?rs=1&pid=ImgDetMain"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="h4" >
+          Cappuccino
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing style={{display:"flex" , justifyContent:"space-between"}}>
+        <h1>{plus}$</h1>
+        
+       <div style={{display:"flex"}}>
+
+        <IconButton aria-label="add to favorites" onClick={()=>{
+          if (plus>2) {
+            setPlus(plus-2)
+            setSum(sum-1)
+          }
+        }}>
+        <RemoveCircleIcon style={{color:purple[500] , fontSize:"50px"}}/>
+        </IconButton>
+        <Typography variant="h3" >
+          {sum}
+        </Typography>
+        <IconButton aria-label="add to favorites" onClick={()=>{
+          if (plus<20) {
+            setPlus(plus+2)
+            setSum(sum+1)
+          }
+        }}>
+          <AddCircleIcon style={{color:purple[500] , fontSize:"50px"}}/>
+        </IconButton>
+        </div>
+      </CardActions>
+      
+    </Card>
     </div>
   );
 }
